894ff6ffb20781fd63e2a8ee0c820a20e282b1ac
HackYourFuture <hackyourfuture@HackYourFutures-MacBook-Air.local>
2023-09-02 17:47:57 +0200

    first update of prep exercises

--- Week3/prep-exercises/1-hyf-program/1-find-mentors.js
+++ Week3/prep-exercises/1-hyf-program/1-find-mentors.js
@@ -1,26 +1,19 @@
 import { modules, students, mentors, classes } from "./hyf.js";
 
-/**
- * Tjebbe would like help to get a list of possible mentors for a module.
- * Fill in this function that finds all the mentors that can teach the given module.
- *
- * It should return an array of names. So something like:
- *  ['John', 'Mary']
- */
-const possibleMentorsForModule = (moduleName) => {
-  // TODO complete this function
+export const possibleMentorsForModule = (moduleName) => {
+  const freeMentors = mentors
+    .filter((mentor) => {
+      return mentor.canTeach.includes(moduleName);
+    })
+    .map((mentor) => {
+      return mentor.name;
+    });
+  return freeMentors;
 };
-// You can uncomment out this line to try your function
-// console.log(possibleMentorsForModule('using-apis'));
 
-/**
- * Tjebbe wants to make it even easier for himself.
- * Fill in this function that chooses a random mentor to teach the given module.
- *
- * It should return a single name.
- */
 const findMentorForModule = (moduleName) => {
-  // TODO complete this function
+  const freeMentors = possibleMentorsForModule(moduleName);
+  const random = Math.floor(Math.random() * freeMentors.length);
+  return freeMentors[random];
 };
-// You can uncomment out this line to try your function
-// console.log(findMentorForModule('javascript'));
+console.log(findMentorForModule("react"));
--- Week3/prep-exercises/1-hyf-program/2-class-list.js
+++ Week3/prep-exercises/1-hyf-program/2-class-list.js
@@ -1,21 +1,39 @@
 import { modules, students, mentors, classes } from "./hyf.js";
-
+import { possibleMentorsForModule } from "./1-find-mentors.js";
 /**
  * We would like to have a list of everyone that is currently participating in a class.
  * This means the students, but also the mentors that are currently teaching the class.
  * The students should be self explanatory, but to find the mentors you will need to follow these steps:
  * - Check what the `currentModule` of the class is
  * - Find the mentor(s) that are `nowTeaching` that module
+ * if it is active return
+ * current module
  *
  * Should return the list of names and their roles. So something like:
  *
  *  [{ name: 'John', role: 'student' }, { name: 'Mary', role: 'mentor' }]
  */
 const getPeopleOfClass = (className) => {
-  // TODO complete this function
+  // to find the mentor i hv to first get what the current class course is then i will find the tr
+  const getCourse = className.map((currentModule) => {
+    return currentModule.currentModule;
+  });
+  // find mentor of each course
+  for (let i = 0; i < getCourse.length; i++) {
+    let boo = getCourse[i];
+    const currentMentor = mentors
+      .filter((mentor) => {
+        return mentor.nowTeaching.includes(boo);
+      })
+      .map((mentor) => {
+        return mentor.name;
+      });
+    console.log(getCourse[i], ":", currentMentor);
+  }
+  console.log(getCourse);
 };
 // You can uncomment out this line to try your function
-// console.log(getPeopleOfClass('class34'));
+// console.log(getPeopleOfClass("class34"));
 
 /**
  * We would like to have a complete overview of the current active classes.
@@ -31,6 +49,14 @@ const getPeopleOfClass = (className) => {
  */
 const getActiveClasses = () => {
   // TODO complete this function
+  const getClasses = classes.filter((cla) => {
+    return cla.active == true;
+  });
+  // .map(({cla}) => {
+  //   return {cla.name};
+  // });
+  // console.log(getClasses);
+  getPeopleOfClass(getClasses);
 };
 // You can uncomment out this line to try your function
-// console.log(getActiveClasses());
+console.log(getActiveClasses());
